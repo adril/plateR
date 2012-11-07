0609459cabef0d9007c5a894f9c380698bc1d90d
adril <adrien.guffens@epitech.eu>
2012-11-07 16:40:28 +0800

    update

--- Common/Message/Message/Message.cpp
+++ Common/Message/Message/Message.cpp
@@ -1,78 +1,10 @@
 // This is the main DLL file.
 
 #include "stdafx.h"
+
 #include "Message.h"
 
-//#include "../include/Message.hpp"
-
 Message::Message() {
-  this->data_ = new char[sizeof(Header) + 1];
+	//return;
 }
 
-Message::Message(Message const &other) {
-  data_ = other.getData();
-  bodyLength_ = other.getBodyLength();
-  login_ = new char[LOGIN_SIZE];
-  memset(login_, '\0', LOGIN_SIZE);
-  strcpy(login_, other.getLogin());
-  type_ = other.getType();
-}
-
-Message::~Message() {
-  // delete data_;
-}
-
-char * Message::getLogin() const {
-  return login_;
-}
-
-char* Message::getData() const {
-  return data_;
-}
-
-char Message::getType() const {
-  return this->type_;
-}
-
-size_t Message::getBodyLength() const {
-  return this->bodyLength_;
-}
-
-size_t Message::getLength() const {
-    return this->bodyLength_ + sizeof(Header);
-}
-
-/* Header's functions */
-
-void Message::decodeHeader() {
-  bodyLength_ = ((Header*)data_)->size_struct;
-  type_ = ((Header*)data_)->command;
-  login_ = new char[LOGIN_SIZE];
-  std::memset(login_, '\0', LOGIN_SIZE);
-  std::strcpy(login_, ((Header*)data_)->login_from);
-  delete[] data_;
-  data_ = new char[bodyLength_ + 1];
-}
-
-void Message::encodeHeader(char type, const char *login) {
-  int assoType[4] = { sizeof(Connection), sizeof(Friend), sizeof(Txt), sizeof(Call) };
-  Header tmp;
-
-  tmp.command = type;
-  tmp.size_struct = assoType[(int)type];
-  std::memset(tmp.login_from, '\0', LOGIN_SIZE);
-  std::strcpy(tmp.login_from, login);
-  delete[] this->data_;
-  login_ = new char[LOGIN_SIZE];
-  std::memset(login_, '\0', LOGIN_SIZE);
-  std::strcpy(login_, login);
-  this->data_ = new char[sizeof(Header) + assoType[(int)type] + 1];
-  std::memcpy(this->data_, &tmp, sizeof(tmp));
-}
-
-/* ******************* */
-
-/* Body's Functions */
-
-void Message::encodeBody() {
-}
--- Common/Message/Message/Message.h
+++ Common/Message/Message/Message.h
@@ -1,48 +1,14 @@
 // Message.h
 
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <deque>
-#include "VSP.hh"
-
-#ifndef __MESSAGE_HPP__
-#define __MESSAGE_HPP__
-
 #pragma once
 
 using namespace System;
 
-namespace Message1 {
+namespace Message {
 
-	public ref class Class1
+	private ref class Message
 	{
-		// TODO: Add your methods for this class here.
-	public:
 		Message();
-		Message(Message const &);
-		virtual ~Message();
-
-		char*		getData() const;
-		char*         getLogin() const;
-		size_t	getLength() const;
-		char		getType() const;
-		size_t	getBodyLength() const;
-
-		void		encodeHeader(char, const char *login);
-		void		decodeHeader(void);
-
-		virtual void	encodeBody();
-
-	protected:
-		size_t	bodyLength_;
-		char		*data_;
-		char		type_;
-		char		*login_;
-	public:
-		std::deque<std::string> list;
+		// TODO: Add your methods for this class here.
 	};
 }
-
-#endif /* !__MESSAGE_HPP__ */
